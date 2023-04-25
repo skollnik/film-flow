21d58beb84feef844d5d3771093d3158afd63984
Dobrica Skolnik <dobrica.skolnik00@gmail.com>
2023-04-25 12:03:34 +0200

    update

--- src/helpers/inputValidation.ts
+++ src/helpers/inputValidation.ts
@@ -1,6 +1,6 @@
 export {};
 
-export function validate(type: string, value: string) {
+export function validate(type: string, value: string): boolean {
   if (type === "title" || type === "director") {
     if (value.trim().length < 1) return false;
     return true;
